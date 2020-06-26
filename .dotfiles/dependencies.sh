4b1098673aa5866d7e5f37e4cd82063cc57d9389
imochlo <e.jsbsolis@gmail.com>
2020-06-26 18:40:48 +0800

    added to dependencies

--- .dotfiles/dependencies.sh
+++ .dotfiles/dependencies.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+sudo apt-get curl
 sudo apt-get install xsel # or xclip
 sudo apt-get install silversearcher-ag
+sudo apt-get install zsh
+sudo apt-get install fonts-powerline
+sudo apt install build-essential cmake python3-dev # youcompleteme
+
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh /tmp/.
+chmod u+x /tmp/Anaconda.*sh
+/tmp/./Anaconda3-2020.02-Linux-x86_64.sh
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/plugins/fzf
+~/.local/plugins/fzf/./install
